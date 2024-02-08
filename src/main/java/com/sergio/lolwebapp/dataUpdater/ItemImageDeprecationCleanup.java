56b35905f789975db3c823c6d5809c981bc602dc
Sergio Hernandez <ehernandez.sergio@gmail.com>
2024-02-07 21:47:30 -0800

    Created item image deprecation cleanup java file

---
+++ src/main/java/com/sergio/lolwebapp/dataUpdater/ItemImageDeprecationCleanup.java
@@ -0,0 +1,46 @@
+package com.sergio.lolwebapp.dataUpdater;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+// This file deletes all files in images folder if not in item.json
+public class ItemImageDeprecationCleanup {
+    final static String ITEM_IMAGES_FOLDER_PATH = "src/main/resources/static/itemImages/";
+    final static String ITEM_JSON_FILE_PATH = "src/main/resources/static/item.json";
+    public static void main(String[] args) {
+        Set<String> itemIds = getItemIdsFromJson();
+
+        File folder = new File(ITEM_IMAGES_FOLDER_PATH);
+        File[] files = folder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".png")) {
+                    String fileName = file.getName().replace(".png", "");
+                    if (!itemIds.contains(fileName)) {
+                        // Delete the file if its name doesn't match any item ID
+                        file.delete();
+                        System.out.println("Deleted: " + file.getName());
+                    }
+                }
+            }
+        }
+    }
+
+    private static Set<String> getItemIdsFromJson() {
+        Set<String> itemIds = new HashSet<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode rootNode = objectMapper.readTree(new File(ITEM_JSON_FILE_PATH));
+            JsonNode dataNode = rootNode.path("data");
+            dataNode.fields().forEachRemaining(entry -> itemIds.add(entry.getKey()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return itemIds;
+    }
+}
--- src/main/java/com/sergio/lolwebapp/dataUpdater/ItemJsonDataUpdater.java
+++ src/main/java/com/sergio/lolwebapp/dataUpdater/ItemJsonDataUpdater.java
@@ -6,8 +6,7 @@ import com.sergio.lolwebapp.controller.DataProperty;
 import com.sergio.lolwebapp.controller.Item;
 import lombok.extern.java.Log;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
@@ -17,46 +16,51 @@ import java.util.Map;
 // THIS FILE CALLS RIOT API AND REMOVES UNWANTED INFORMATION
 // (SUCH AS DEPRECATED ITEMS) FROM THE JSON
 public class ItemJsonDataUpdater {
-    final static String URL_STRING = "https://ddragon.leagueoflegends.com/cdn/14.3.1/data/en_US/item.json";
-    final static String JSON_FILE_PATH = "src/main/resources/static/item.json";
+    // Make sure to use latest version link for item json
+    final static String ITEM_DATA_LOL_API_URL_STRING = "https://ddragon.leagueoflegends.com/cdn/14.3.1/data/en_US/item.json";
+    final static String ITEM_JSON_FILE_PATH = "src/main/resources/static/item.json";
     public static void main(String[] args) throws IOException {
         // TODO: how to run this program in terminal (that's how I will run it later on).
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = getLeagueApiJsonData(objectMapper);
-
-        Map<String, Item> filteredItems = new HashMap<>(); // Stores only in-game items
-        JsonNode dataNode = rootNode.get("data");
-        if (dataNode != null && dataNode.isObject()) {
-            dataNode.fields().forEachRemaining(entry -> {
-                String itemId = entry.getKey();
-                JsonNode itemNode = entry.getValue();
-                // IN-GAME ITEM CRITERIA: maps:11 is true, or maps:11 isn't false,
-                // or maps:11 is true and inStore doesn't exist
-                boolean isInMap = itemNode.has("maps") && itemNode.get("maps").get("11").asBoolean();
-                boolean inStore = itemNode.has("inStore");
-
-                if (isInMap && !inStore) {
-                    try {
-                        Item item = objectMapper.treeToValue(itemNode, Item.class);
-                        filteredItems.put(itemId, item);
-                    } catch (IOException e) {
-                        log.warning("Error processing item: " + itemId);
-                    }
-                }
-            });
-        }
-
-        DataProperty result = new DataProperty();
-        result.setData(filteredItems);
-
-        objectMapper.writeValue(new File(JSON_FILE_PATH), result);
+        saveLeagueApiJsonDataToItemsFile();
     }
 
-   public static JsonNode getLeagueApiJsonData(ObjectMapper objectMapper) throws IOException {
-       URL url = new URL(URL_STRING);
+    public static JsonNode getLeagueApiJsonData(ObjectMapper objectMapper) throws IOException {
+       URL url = new URL(ITEM_DATA_LOL_API_URL_STRING);
        HttpURLConnection con = (HttpURLConnection) url.openConnection();
        con.setRequestMethod("GET");
        return objectMapper.readTree(con.getInputStream());
-   }
+    }
+
+    public static void saveLeagueApiJsonDataToItemsFile() throws IOException {
+
+       ObjectMapper objectMapper = new ObjectMapper();
+       JsonNode rootNode = getLeagueApiJsonData(objectMapper);
+
+       Map<String, Item> filteredItems = new HashMap<>(); // Stores only in-game items
+       JsonNode dataNode = rootNode.get("data");
+       if (dataNode != null && dataNode.isObject()) {
+           dataNode.fields().forEachRemaining(entry -> {
+               String itemId = entry.getKey();
+               JsonNode itemNode = entry.getValue();
+               // IN-GAME ITEM CRITERIA: maps:11 is true, or maps:11 isn't false,
+               // or maps:11 is true and inStore doesn't exist
+               boolean isInMap = itemNode.has("maps") && itemNode.get("maps").get("11").asBoolean();
+               boolean inStore = itemNode.has("inStore");
+
+               if (isInMap && !inStore) {
+                   try {
+                       Item item = objectMapper.treeToValue(itemNode, Item.class);
+                       filteredItems.put(itemId, item);
+                   } catch (IOException e) {
+                       log.warning("Error processing item: " + itemId);
+                   }
+               }
+           });
+       }
+
+       DataProperty result = new DataProperty();
+       result.setData(filteredItems);
+
+       objectMapper.writeValue(new File(ITEM_JSON_FILE_PATH), result);
+    }
 }
