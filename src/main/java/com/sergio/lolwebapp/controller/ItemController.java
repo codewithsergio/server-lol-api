d835f73321460634451628895266b435ecadaa41
Sergio Hernandez <ehernandez.sergio@gmail.com>
2024-03-03 04:08:30 -0800

    added CrossOrigin to abide by js fetch rules

--- src/main/java/com/sergio/lolwebapp/controller/ItemController.java
+++ src/main/java/com/sergio/lolwebapp/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.sergio.lolwebapp.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sergio.lolwebapp.entity.DataProperty;
 import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import java.io.IOException;
 
 @RestController
 @Log
+@CrossOrigin(origins = "http://127.0.0.1:5501") // Added to abide by javascript fetch rules
 public class ItemController {
     @GetMapping(path = "/items")
     public DataProperty items() throws IOException {
